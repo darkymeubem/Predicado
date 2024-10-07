47cbeffe2198a8f56697aa9e317ed3ffa09c0e2f
Felipe Pedroza <felipelp2910@gmail.com>
2024-10-07 15:27:38 -0300

    Interface implementation

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,30 +1,25 @@
 package org.example.application;
 
 import org.example.entities.Product;
-import org.example.utill.ProductPedicate;
+import org.example.utill.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
         List<Product> list = new ArrayList<>();
         list.add(new Product("Tv", 900.00));
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        double min = 100.0;
+        list.forEach(new PriceUpdate());
 
-        //Remover da lista todo produto que tiver preço menor que 100
-
-
-        list.removeIf(p -> p.getPrice() <= min);
-
-        for(Product p: list){
-            System.out.println(p);
-        }
+        list.forEach(System.out::println);
 
     }
 }
